64e5bd06a974d0ffec9ff402def41f620cbb660d
Matias Galeano <99203645+matigaleanodev@users.noreply.github.com>
2026-02-01 20:39:33 -0300

    feat(ui): creacion de header y footer

--- src/app/layout/footer/footer.component.ts
+++ src/app/layout/footer/footer.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { LucideAngularModule, ArrowUp } from 'lucide-angular';
 
 @Component({
   selector: 'layout-footer',
-  imports: [],
+  imports: [LucideAngularModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  readonly arrowUpIcon = ArrowUp;
 
+  readonly notion = signal(
+    'https://matigaleano-dev.notion.site/Personal-Site-2faaa07ef7dd8008a753d336af47e26f',
+  );
+  readonly linkedin = signal('https://www.linkedin.com/in/matigaleanodev/');
+  readonly github = signal('https://github.com/matigaleanodev/');
+
+  backToTop() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  }
 }
